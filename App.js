4add3e71c58c21cf7b59322d0b0a9225d325c646
wanghong <wanghong@fenghuantech.com>
2018-12-10 11:44:05 +0800

    将react 第三方依赖包打成aar 并将第三方依赖中的依赖包添加到 app gradle文件中

--- App.js
+++ App.js
@@ -7,9 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import Home from "./src/component/Home";
-import {ActionConst, Router, Scene, Tabs, Drawer} from 'react-native-router-flux';
+import {StyleSheet} from 'react-native';
+import { Router, Scene} from 'react-native-router-flux';
 import ChattingScreen from './src/component/ChattingScreen'
 
 class App extends Component {
@@ -23,35 +22,6 @@ class App extends Component {
             <Router>
                 <Scene  key={'root'}>
                     <Scene key="home" component={ChattingScreen} hideNavBar={true}/>
-
-                    {/*<Tabs key="tabbar" swipeEnabled tabBarPosition="bottom" showLabel={false}*/}
-                          {/*tabBarStyle={styles.tabBarStyle}*/}
-                    {/*>*/}
-                        {/*<Scene key="home" component={Home} hideNavBar={true} title="收款"*/}
-                               {/*icon={TabIcon}*/}
-                               {/*iconDefaultImage={require('./image/iv_receipt_default.png')}*/}
-                               {/*iconSelectImage={require('./image/iv_receipt_select.png')}*/}
-                               {/*type={ActionConst.RESET}*/}
-                               {/*panHandlers={null}*/}
-                        {/*/>*/}
-                        {/*<Scene key="pay" component={ PayPage } hideNavBar={true} title="支付"*/}
-                               {/*iconDefaultImage={require('./image/iv_pay_default.png')}*/}
-                               {/*iconSelectImage={require('./image/iv_pay_select.png')}*/}
-                               {/*icon={TabIcon}/>*/}
-                        {/*<Scene key="shopping" component={ShoppingPage} hideNavBar={true} title="商城"*/}
-                               {/*iconDefaultImage={require('./image/iv_mall_default.png')}*/}
-                               {/*iconSelectImage={require('./image/iv_mall_select.png')}*/}
-                               {/*icon={TabIcon}*/}
-                        {/*/>*/}
-                        {/*<Scene key="user" component={UserPage} hideNavBar={true} title="用户"*/}
-                               {/*iconDefaultImage={require('./image/iv_user_default.png')}*/}
-                               {/*iconSelectImage={require('./image/iv_user_select.png')}*/}
-                               {/*icon={TabIcon}*/}
-                        {/*/>*/}
-
-                    {/*</Tabs>*/}
-
-
                 </Scene>
             </Router>
         );
